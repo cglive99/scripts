62024b5d7820f85776308ececab43d9579a61b24
Chris Griffin <cglive99@CAir.local>
2017-05-29 17:21:12 -0400

    Removed OneNote files

--- delete files older than 10 days.ps1
+++
@@ -1,7 +0,0 @@
-# Delete all Files in E:\inetpub\logs\Logfiles\FTPSVC2 older than 10 day(s)
-$Path = "E:\inetpub\logs\Logfiles\FTPSVC2"
-$Daysback = "-10"
- 
-$CurrentDate = Get-Date
-$DatetoDelete = $CurrentDate.AddDays($Daysback)
-Get-ChildItem $Path | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item
--- delete files older then 30 days.ps1
+++
@@ -1,7 +0,0 @@
-# Delete all Files in D:\Import\PlantsComplete older than 30 day(s)
-$Path = "D:\Import\PlantsComplete"
-$Daysback = "-30"
- 
-$CurrentDate = Get-Date
-$DatetoDelete = $CurrentDate.AddDays($Daysback)
-Get-ChildItem $Path | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item
